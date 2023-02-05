2f605950d822b99ee48574e9c40e4c0817776c08
yip-i <75112256+yip-i@users.noreply.github.com>
2023-02-04 17:28:49 -0800

    Added YAML file as the command argument was too long. Editied run_data_prep.py to interact with the YAML file.

--- run_data_prep.py
+++ run_data_prep.py
@@ -9,6 +9,7 @@ import argparse
 import logging
 import torchaudio
 import numpy as np
+import yaml
 
 from generate_directory_list import generate_directory_uaspeech
 from rename_datasets import match_speakers
@@ -17,8 +18,8 @@ def get_data_prep_args():
     parser = argparse.ArgumentParser(description="Prepares UASpeech for VS or ASR")
 
     # "./data/UASpeech/audio/original/*/*.wav"
-    parser.add_argument("-p", "--file_path", help="File path to UASpeech. ex: /data/UASpeech/audio/original/*/*.wav")
-    parser.add_argument("-x", "--excel_file_path", help="File path to UAspeech excel mapping word ID to transcript")
+    # parser.add_argument("-p", "--file_path", help="File path to UASpeech. ex: /data/UASpeech/audio/original/*/*.wav")
+    # parser.add_argument("-x", "--excel_file_path", help="File path to UAspeech excel mapping word ID to transcript")
 
     parser.add_argument("-r", "--create_transcript", help="--regenerate UAspeech transcript file",choices=["Y", "N", "y", "n"], default="N")
     parser.add_argument("-t", "--target_speaker", help="Target speaker to generate mapping for", required=True)
@@ -33,11 +34,17 @@ def get_data_prep_args():
 
 def main():
     args = get_data_prep_args()
+    with open('config.yaml', 'r') as f:
+        doc = yaml.load(f)
+
+    excel_file_path = doc["file_paths"]["excel_file_path"]
+    file_path = doc["file_paths"]["data_file_path"]
+
     if not os.path.isfile("UAspeech_transcripts.csv") or args.create_transcript.lower() == "y":
-        if args.file_path == None or args.excel_file_path == None:
+        if file_path == None or excel_file_path == None:
             raise FileNotFoundError("Path to audio file and path to excel file needed")
         else:
-            generate_directory_uaspeech(args.file_path, args.excel_file_path)
+            generate_directory_uaspeech(file_path, excel_file_path)
 
     df = pd.read_csv("UAspeech_transcripts.csv")
     target_speaker = str(args.target_speaker).strip()
@@ -48,7 +55,7 @@ def main():
         match_speakers(target_speaker,df=df , random_seed=args.random_seed, src_speaker=args.source_speaker)
 
 
-    print(args.file_path)
+    print(file_path)
 
 if __name__ =="__main__":
     main()
